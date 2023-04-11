6b22da6b32e70a452939961c545b5460baa590b7
utopian-monkey <narang.amal06@gmail.com>
2023-04-11 19:47:56 +0530

    adds address field in student add page

--- models/students.js
+++ models/students.js
@@ -7,6 +7,7 @@ const Students = new Schema({
     name: {type: String, required :true },
     fathers_name:{type:String},
     contact_no:{type: Number},
+    address:{type: String},
     class:{type:Number, min:1,max:10},
     gender:{type: String, enum:["boy","girl"]},
     issued_books:[{type:Schema.Types.ObjectId, ref:"Books"}]
